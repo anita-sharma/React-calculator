d484f2ce870ded94f484a9adb134ff5c42203f51
anita-sharma <2016uce1171@mnit.ac.in>
2020-07-24 13:01:48 +0530

    Updates

---
+++ precache-manifest.8558707cd549748b21f56694e247c622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6409cff65b22449fb982d9ebe162466",
+    "url": "/React-calculator/index.html"
+  },
+  {
+    "revision": "41c5c7e301cbdc6a43bc",
+    "url": "/React-calculator/static/css/main.cb2f8340.chunk.css"
+  },
+  {
+    "revision": "c43374801049cc8b6cfc",
+    "url": "/React-calculator/static/js/2.0804e751.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-calculator/static/js/2.0804e751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c5c7e301cbdc6a43bc",
+    "url": "/React-calculator/static/js/main.b2dc7112.chunk.js"
+  },
+  {
+    "revision": "436eec30c65cc1dd148b",
+    "url": "/React-calculator/static/js/runtime-main.f6b6bc55.js"
+  }
+]);
